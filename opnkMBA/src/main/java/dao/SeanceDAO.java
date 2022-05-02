4b30c5e717f6f04e25c7ef8371be59e974d70a95
bonouo <antoinebon88@gmail.com>
2022-05-02 10:30:29 +0200

    ajout de la sceance.dao

--- opnkMBA/src/main/java/dao/SeanceDAO.java
+++ opnkMBA/src/main/java/dao/SeanceDAO.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedList;
 
+import openbd.Cours;
+import openbd.Salle;
 import openbd.Seance;
 
 
@@ -45,6 +47,7 @@ public class SeanceDAO extends DAO<Seance> {
 			if (rs.next()) { //si rs.next est false alors l'id n'as pas été generé comme il le faut 
 				seance.setId(rs.getInt(1));
 			}
+			
 		} catch (SQLException e) {
 			succes = false;
 			e.printStackTrace();//printStackTrace() affiche l'exeption dans la console 
@@ -67,16 +70,16 @@ public class SeanceDAO extends DAO<Seance> {
 		}
 		return succes;
 	}
-	//TODO FAIRE L'UPDATE DE LA CLASSE SCEANCE singelton
+	//TODO a vérifier 
 	@Override
 	public boolean update(Seance seance) {
 		boolean succes = true;
 		try {			
 			String requete = "UPDATE "+TABLE+" ("+DATEDEB+", "+DATEFIN+","+IDSALLE+","+IDCOURS+") VALUES (?, ?, ?) WHERE ("+CLE_PRIMAIRE+" = ?"+")";
 			PreparedStatement pst = Connexion.getInstance().prepareStatement(requete);
-			pst.setString(1, seance.getDateDeb());
-			pst.setBoolean(2, seance.getDateFin());
-			pst.setInt(3, seance.getSalle().getId());	
+			pst.setDate(1, seance.getDateDeb());
+			pst.setDate(2, seance.getDateFin());
+			pst.setInt(3, seance.getSalle().getId());
 			pst.setInt(4, seance.getCours().getId());
 			pst.executeUpdate();// on execute la requete qui consiste a mettre a jour un enregistrement de la table 
 		} catch (SQLException e) {
@@ -87,9 +90,9 @@ public class SeanceDAO extends DAO<Seance> {
 	}
 	@Override
 	public Seance read(int id) {
-		Seance salle = null;
+		Seance seance = null;
 		try {			
-			String requete = "SELECT "+CLE_PRIMAIRE+", "+DENOMINATION+", "+EQUIPEMENTVIRTUEL+", "+NBPLACES+"+ FROM "+TABLE+" WHERE ("+CLE_PRIMAIRE+" = ?"+")";
+			String requete = "SELECT "+CLE_PRIMAIRE+", "+DATEDEB+", "+DATEFIN+", "+IDSALLE+", "+IDCOURS+" + FROM "+TABLE+" WHERE ("+CLE_PRIMAIRE+" = ?"+")";
 			PreparedStatement pst = Connexion.getInstance().prepareStatement(requete);
 			pst.setInt(1, id);
 
@@ -103,12 +106,18 @@ public class SeanceDAO extends DAO<Seance> {
 				rs.close();
 				return null;
 			}
-			salle = new Seance(rs.getInt(CLE_PRIMAIRE), rs.getString(DENOMINATION), rs.getBoolean(EQUIPEMENTVIRTUEL), rs.getInt(NBPLACES));	 
+			// vas chercher l'adresse memoire des deux objets 
+			
+			Salle salle = SalleDAO.getInstance().read(rs.getInt(IDSALLE));
+			Cours cours = CoursDAO.getInstance().read(rs.getInt(IDCOURS));
+			
+			//TODO peut etre a changer les parametres de dates car il son peut etre faux 
+			seance = new Seance(rs.getInt(CLE_PRIMAIRE), rs.getDate(DATEDEB), rs.getDate(DATEFIN), salle, cours);	 
 			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return salle;
+		return seance;
 	}
 
 }
